1821154896de24d5acd7370e4a4ef5e028bbe571
Ordgod <972931271@qq.com>
2018-11-09 21:02:39 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Jingnan Jia
+@contact: jiajingnan2222@gmail.com
+@file: test1.py.py
+@time: 18-11-9 09 下午8:49
+'''
+
+# this file is to test how to use git in pycharm.
+import numpy as np
+
+a = np.ones((2,2))
+print(a)
+
