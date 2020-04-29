ffc8f1073f83a9af5a6ca6f25b24afb223b69977
jjia <972931271@qq.com>
2020-04-29 13:24:05 +0200

    this is for test2, add vcs.xml and .gitignore to VCS system

---
+++ test2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Jingnan Jia
+@contact: jiajingnan2222@gmail.com
+@file: test1.py.py
+@time: 18-11-9 09 下午8:49
+'''
+
+import tensorflow as tf
